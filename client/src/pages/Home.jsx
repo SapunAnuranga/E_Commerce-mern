20c413e1c278fcf9fd74ed4c0e9d63f3c85c425f
SAPUN ANURANGA <sapunanuranga98@gmail.com>
2025-04-27 21:15:01 +0530

    Added BestSeller and LatestCollection sections with dynamic product listing and reusable components.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Hero from '../components/Hero';
+import LatestCollection from '../components/LatestCollection';
+import BestSeller from '../components/bestSeller';
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <LatestCollection/>
+      <BestSeller/>
     </div>
   );
 };
